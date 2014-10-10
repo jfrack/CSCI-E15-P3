077f84908e5abe2517171e5ca91c96e318398cf5
Jarek Frackowiak <jarektf@gmail.com>
2014-10-10 17:29:47 -0500

    Added splash page

---
+++ app/views/header.php
@@ -0,0 +1,15 @@
+<head>
+	<title>CSCI E-15: P3 - Laravel Basics</title>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta name="keywords" content="lorem ipsum random user generator">
+	<meta name="description" content="P3 - Developer's Best Friend">
+	<!-- Get jquery from CDN -->
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<!-- Get Bootstrap from CDN -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<!-- Get Bootswatch theme from CDN -->
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.2.0/readable/bootstrap.min.css">
+	<!-- Get local CSS -->
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
---
+++ app/views/splash.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<?php include('header.php'); ?>
+	<body>
+		<h1>P3 - Developer's Best Friend</h1>
+		<?php //include('content.php'); ?>
+	</body>
+</html>
