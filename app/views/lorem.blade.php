a95ca56f6771a38ff5faebe7c6fba2f7bd80bb63
Jarek Frackowiak <jarektf@gmail.com>
2014-10-24 16:39:04 -0500

    URL fix for prod server

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -14,9 +14,10 @@
 		</div>
 
 		<!-- include JS from ZeroClipboard package -->
-    	<script type="text/javascript" src="../vendor/zeroclipboard/zeroclipboard/dist/ZeroClipboard.js"></script>
+    	<!-- <script type="text/javascript" src="../vendor/zeroclipboard/zeroclipboard/dist/ZeroClipboard.js"></script> -->
+    	<script type="text/javascript" src={{ URL::to('../vendor/zeroclipboard/zeroclipboard/dist/ZeroClipboard.js') }}></script>
     	<!-- and also custom JS for the "Copy to Clipboard" button logic -->
-    	<script type="text/javascript" src="js/copy_clip.js"></script>
+    	<script type="text/javascript" src={{ URL::to('js/copy_clip.js') }}></script>
 	@endif
 	<br>
 </body>
